dd60f9be3ee4347055c267d44c7df0fd26865a1a
David Scherer-ODell <dscherer21@gmail.com>
2020-05-26 22:00:26 -0500

    adding more quiz questions

--- client/src/components/questions.js
+++ client/src/components/questions.js
@@ -54,6 +54,94 @@ const questions = [
         answer: "An Electric Rice Cooker",
         explanation: "Rice Cookers seem to be a common vessel for the Evil Containment Wave. After Mutaito seals away King Piccolo, he then passes away himself. Master Roshi then hides the container at the bottom of the ocean, so that King Piccolo can never be released."
     },
+    {
+        question: "In Dragon Ball Z, during the Saiyan Saga, what causes Vegeta to, finally, decide to leave earth?",
+        choices: [
+            "He loses to Goku in an Energy Beam showdown.",
+            "Yajirobe cuts his tail off.",
+             "Piccolo hits him with his Special Beam Cannon.",
+            "Gohan, in his Great Ape form, falls on top of him."
+        ],
+        answer: "Gohan, in his Great Ape form, falls on top of him.",
+        explanation: "Gohan transforms into his Great Ape form, when he looks up at Vegeta's energy ball still up in the sky. Vegeta tries to fight him, but is too weakened to be effective. Vegeta does manage to get off an energy disk that cuts Gohan's tail off, but unfortunately for the Saiyan Prince, Gohan does not transform back to normal before landing on top of him."
+    },
+    {
+        question: "In Dragon Ball Z, during the Namek Saga, how does Vegeta recover from being beaten by Zarbon?",
+        choices: [
+            "Zarbon put Vegeta in a Rejuvenation Chamber.",
+            "Gohan finds Vegeta and gives him a Senzu Bean.",
+             "Dende heals him.",
+            "Vegeta crawls to a nearby cave, where he waits til he is healed."
+        ],
+        answer: "Zarbon put Vegeta in a Rejuvenation Chamber.",
+        explanation: "At first, Zarbon left Vegeta for dead, but came back to collect Vegeta after Frieza ordered him to. Zarbon put Vegeta in a Rejuvenation Chamber, so they could question him about the Dragon Balls and discuss his betrayal of Frieza after he recovered."
+    },
+    {
+        question: "In Dragon Ball Z, during the Android Saga when Gohan was carrying Bulma, Baby Trunks, and Yajirobe back to Capsule Corp, why did they have to stop?",
+        choices: [
+            "They stopped so they could have lunch.",
+            "Piccolo called out to Gohan telepathically that he was in trouble, so Gohan left to help him.",
+            "Baby Trunks peed on Yajirobe.",
+            "They stopped so Gohan could help a hunter that had been attacked by Android 20."
+        ],
+        answer: "Baby Trunks peed on Yajirobe.",
+        explanation: "On the way to Capsule Corp, Bulma comments on how quiet Trunks was being and holds him up. At this point, Trunks' diaper leaks all over Yajirobe's head and he freaks out. They stop so Bulma can change Trunks and Yajirobe can clean up."
+    },
+    {
+        question: "In Dragon Ball Z, on the day of the Cell Games, who was the first person to fight Cell?",
+        choices: [
+            "Goku",
+            "Mr. Satan",
+            "Android 16",
+            "Caroni"
+        ],
+        answer: "Caroni",
+        explanation: "Right before Hercule could challenge Cell, students of his fighting dojo, Caroni and Pirozshki, showed up to challenge. Caroni was the first challenger. When Caroni tried to go for an aerial attack at Cell, he was quickly defeated when Cell raised his power to launch Caroni high in sky and landing outside the ring."
+    },
+    {
+        question: "In Dragon Ball Z, how did Krillin and Piccolo break free of Dabura's petrification spit?",
+        choices: [
+            "Videl's feelings for Gohan made Dabura realize that he wanted to become a good person.",
+            "Vegeta grew impatient with Gohan's lack of training, and killed Dabura himself.",
+            "Majin Buu turned Dabura into a cookie and ate him.",
+            "Goten and Trunks used a vial of Sacred Water given to them by Dende to revive them."
+        ],
+        answer: "Majin Buu turned Dabura into a cookie and ate him.",
+        explanation: "Supreme Kai stated that Dabura would need to die to break the petrification. Later, when Dabura realized that Majin Buu was unstable and tried to attack him, Buu turned Dabura into a cookie and ate him. This caused the petrification on Krillin and Piccolo to be released."
+    },
+    {
+        question: "In Dragon Ball GT, who kills Dolltaki?",
+        choices: [
+            "Goku",
+            "Lord Luud",
+            "General Rilldo",
+            "Bon Para"
+        ],
+        answer: "General Rilldo",
+        explanation: "After escaping from Lord Luud, Dolltaki attempts to report to Dr. Myuu about Luud's destruction, but is confronted by General Rilldo who kills him with a Ki Blast."
+    },
+    {
+        question: "In Dragon Ball GT, how does Mr. Satan avoid being infected by Baby?",
+        choices: [
+            "Baby delights in turning the world against it's savior.",
+            "He hides in an underground bunker in his mansion.",
+            "He is absorbed by Majin Buu.",
+            "He pretends to be infected and is overlooked by Baby."
+        ],
+        answer: "He is absorbed by Majin Buu.",
+        explanation: "Buu is immune to Baby's mind control infection, and, in order to protect him, hides Mr. Satan in one of his pods inside Buu's body."
+    },
+    {
+        question: "In Dragon Ball GT, how does Mr. Satan avoid being infected by Baby?",
+        choices: [
+            "Baby delights in turning the world against it's savior.",
+            "He hides in an underground bunker in his mansion.",
+            "He is absorbed by Majin Buu.",
+            "He pretends to be infected and is overlooked by Baby."
+        ],
+        answer: "He is absorbed by Majin Buu.",
+        explanation: "Buu is immune to Baby's mind control infection, and, in order to protect him, hides Mr. Satan in one of his pods inside Buu's body."
+    },
 ];
 
 export default questions;
